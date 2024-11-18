9b73ea91b86b08c8ddbbd19e37a595c40e717664
tomsmith <103900032+tomsmith11@users.noreply.github.com>
2024-11-18 14:00:16 +0000

    Created calc.py
    This is fully functioning. Might add a UI to it later

---
+++ calc.py
@@ -0,0 +1,33 @@
+result_prev = 0
+operator = '+'
+
+print('result_prev:', result_prev)
+
+firstNum = input('Enter your first number')
+result_prev = int(firstNum)
+print('result_prev:', result_prev)
+
+while True:
+    operatorInput = input("Enter an operator (+, -, *, /) or press 'q' to quit")
+
+    if operatorInput == 'q': 
+        break
+
+    operator = operatorInput
+
+    num = int(input('Enter a number'))
+    currentNum = int(num)
+
+    if operator == '+':
+        result_prev += currentNum
+    elif operator == '-':
+        result_prev -= currentNum
+    elif operator == '*':
+        result_prev *= currentNum
+    elif operator == '/':
+        result_prev /= currentNum
+    else: 
+        print('invalid operator')
+    print('result_prev:', result_prev)
+
+print('Final result:', result_prev)
